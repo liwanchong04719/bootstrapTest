2c40d2b2c2fa4ff9a98ddd7e083c3dc7457888c4
wangtun <wangtun@navinfo.com>
2016-11-17 19:47:08 +0800

    添加统计图

---
+++ scripts/main.js
@@ -0,0 +1,195 @@
+/**
+ * Created by wangtun on 2016/11/17.
+ */
+$(function () {
+  initPieChart();
+  initBarChart();
+})
+
+function initPieChart(){
+  var dom = document.getElementById("pieChart");
+  var myChart = echarts.init(dom);
+  var app = {};
+  option = null;
+  app.title = '环形图';
+
+  option = {
+    tooltip: {
+      trigger: 'item',
+      formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+      orient: 'vertical',
+      x: 'left',
+      data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+    },
+    series: [
+      {
+        name:'访问来源',
+        type:'pie',
+        radius: ['50%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          normal: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            show: true,
+            textStyle: {
+              fontSize: '30',
+              fontWeight: 'bold'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            show: false
+          }
+        },
+        data:[
+          {value:335, name:'直接访问'},
+          {value:310, name:'邮件营销'},
+          {value:234, name:'联盟广告'},
+          {value:135, name:'视频广告'},
+          {value:1548, name:'搜索引擎'}
+        ]
+      }
+    ]
+  };
+  ;
+  if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+  }
+}
+
+
+function  initBarChart() {
+  var dom = document.getElementById("barChart");
+  var myChart = echarts.init(dom);
+  var app = {};
+  option = null;
+  option = {
+    title: {
+      text: '动态数据',
+      subtext: '纯属虚构'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data:['最新成交价', '预购队列']
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        dataView: {readOnly: false},
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    dataZoom: {
+      show: false,
+      start: 0,
+      end: 100
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: true,
+        data: (function (){
+          var now = new Date();
+          var res = [];
+          var len = 10;
+          while (len--) {
+            res.unshift(now.toLocaleTimeString().replace(/^\D*/,''));
+            now = new Date(now - 2000);
+          }
+          return res;
+        })()
+      },
+      {
+        type: 'category',
+        boundaryGap: true,
+        data: (function (){
+          var res = [];
+          var len = 10;
+          while (len--) {
+            res.push(len + 1);
+          }
+          return res;
+        })()
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        scale: true,
+        name: '价格',
+        max: 30,
+        min: 0,
+        boundaryGap: [0.2, 0.2]
+      },
+      {
+        type: 'value',
+        scale: true,
+        name: '预购量',
+        max: 1200,
+        min: 0,
+        boundaryGap: [0.2, 0.2]
+      }
+    ],
+    series: [
+      {
+        name:'预购队列',
+        type:'bar',
+        xAxisIndex: 1,
+        yAxisIndex: 1,
+        data:(function (){
+          var res = [];
+          var len = 10;
+          while (len--) {
+            res.push(Math.round(Math.random() * 1000));
+          }
+          return res;
+        })()
+      },
+      {
+        name:'最新成交价',
+        type:'line',
+        data:(function (){
+          var res = [];
+          var len = 0;
+          while (len < 10) {
+            res.push((Math.random()*10 + 5).toFixed(1) - 0);
+            len++;
+          }
+          return res;
+        })()
+      }
+    ]
+  };
+  clearInterval(app.timeTicket);
+  app.count = 11;
+  app.timeTicket = setInterval(function (){
+    axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
+
+    var data0 = option.series[0].data;
+    var data1 = option.series[1].data;
+    data0.shift();
+    data0.push(Math.round(Math.random() * 1000));
+    data1.shift();
+    data1.push((Math.random() * 10 + 5).toFixed(1) - 0);
+
+    option.xAxis[0].data.shift();
+    option.xAxis[0].data.push(axisData);
+    option.xAxis[1].data.shift();
+    option.xAxis[1].data.push(app.count++);
+
+    myChart.setOption(option);
+  }, 2100);
+  ;
+  if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+  }
+}
