73b8127bcbad635e40c59fb71e46b815e6df80f8
zhongxiaoming519 <zhongxiaoming@navinfo.com>
2016-11-21 10:13:05 +0800

    部分完成左侧列表

--- scripts/map.js
+++ scripts/map.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
     var height = document.documentElement.clientHeight;
     var width = document.documentElement.clientWidth;
 	$('#map').css("height", height);
-	
-	
+	$('select[name="inverse-select"]').select2({dropdownCssClass: 'select-inverse-dropdown'});
+	//初始化左侧列表
+	initHoouseList();
+	var housedata = house.data.list;
+	$('#left_house_list').bootstrapTable('load',housedata);
 	//地图
 	var map = initmap();
 	//根据地图级别绘制图标
@@ -30,14 +33,8 @@ $(document).ready(function(){
 			}
 		}
 
-
-
 	})
-	
-	
 
-
- 
 
 })
 
@@ -67,4 +64,34 @@ function createMarker(options) {
 		event.target.setZIndex(1000)
 	})
 	return label;
+}
+
+
+//生成房产列表
+
+function initHoouseList(){
+   $('#left_house_list').bootstrapTable({
+        height: (document.documentElement.clientHeight || document.body.clientHeight) - 230,
+        classes:"table table-hover table-condensed",
+        striped:true,
+        toolbar:'#rawdata-toolbar',
+        pagination:true,
+		showHeader:false,
+        showColumns: false,
+        showRefresh:false,
+        showToggle:false,
+		
+        pageSize: 5,
+        columns: [ {
+            field: 'house_code',
+            align: 'center',
+            title: '编号',
+         	formatter:function(value, row){
+                    var html='<div class="row"><a class="tableitem" href="/ershoufang/101100781178.html" target="_blank" title="富力城B区&nbsp;3室1厅" data-community="1111027374273"><div class="item-aside"><img src="house.jpg"></div><div class="item-main"><p class="item-tle">富力城B区&nbsp;3室1厅</p><p class="item-des"><span>3室1厅</span><span data-origin="102.8">102平米</span><span>南 北</span><span class="item-side">1050<span>万</span></span></p><p class="item-community"><span class="item-replace-com" data-origin="">富力城B区</span><span class="item-exact-com">富力城B区</span></p><p class="item-tag-wrap"><span class="item-tag-subway item-extra" title="距离10号线双井46铁</span><span class="item-tag-school item-extra" title="芳草地国际学校双花园校区">学区</span><span class="item-tag-taxfree item-extra" title="">房本满两年</span></p></div></a></div>'
+
+return html;
+
+                }
+        }]
+    })
 }
