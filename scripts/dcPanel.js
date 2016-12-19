ba913f3019b062c50d65f5c5ba12050650104388
李万冲 <liwanchong@navinfo.com>
2016-12-19 23:07:56 +0800

    修改

--- scripts/dcPanel.js
+++ scripts/dcPanel.js
@@ -1,17 +1,16 @@
 /**
  * Created by liwanchong on 2016/12/14.
  */
-var info =
+var dcInfo =
   '<div id="houseInfo" style="height: 400px;width: 450px;overflow: auto;">' +
-  '<h6 style="text-align: center;padding-bottom:3px;margin-top: 5px;margin-bottom: 10px;border-bottom: 1px solid #e8e8e8">房产信息</h6>' +
+  '<h6 style="text-align: center;padding-bottom:3px;margin-top: 5px;margin-bottom: 10px;border-bottom: 1px solid #e8e8e8">地产信息</h6>' +
   '  <img style="width: 100%;height: 200px" src="../lib/FlexSlider/demo/images/kitchen_adventurer_donut.jpg"/>' +
   '<ul class="fields" style="">' +
-  '<li><label>业主：</label><label id="yezhu">---</label></li>' +
-  '<li><label>建筑面积：</label><label id="jzmj">---</label></li>' +
-  '<li><label>业态：</label><label id="yetai">---</label></li>' +
+  '<li><label>土地使用人：</label><label id="tdsyr">---</label></li>' +
+  '<li><label>使用面积：</label><label id="symj">---</label></li>' +
   '<li><label>座落：</label><label id="zuoluo">---</label></li>' +
-  '<li><label>出租率：</label><label id="czl">---</label></li>' +
-  '<li><label>现平均单价：</label><label id="xpjdj">---</label></li>' +
+  '<li><label>土地证编号：</label><label id="tcdbh">---</label></li>' +
+  '<li><label>终止日期：</label><label id="zzdate">---</label></li>' +
   '</ul>' +
   '<div class="wrapper">' +
   '<ul class="items">' +
@@ -22,8 +21,8 @@ var info =
   '<div id="barChart" style="height: 250px;width: 350px">' +
   '</div></li></ul> </li>' +
   '<li>' +
-  '<a href="#">对方房产信息<i class="glyphicon glyphicon-chevron-right"></i></a>' +
-  '<ul class="sub-items" id="dyfcxx">' +
+  '<a href="#">对方地产信息<i class="glyphicon glyphicon-chevron-right"></i></a>' +
+  '<ul class="sub-items" id="dydcxx">' +
   '</ul>' +
   '</li>' +
   ' </ul>' +
